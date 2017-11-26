578ae4ab1e4da49f2dfb16ed3edaf4ba1781cae8
Victorcavendish <33516000+Victorcavendish@users.noreply.github.com>
2017-11-26 00:57:11 -0200

    Update GitNetBeans.java

--- src/gitnetbeans/GitNetBeans.java
+++ src/gitnetbeans/GitNetBeans.java
@@ -5,6 +5,7 @@ public class GitNetBeans {
 
     public static void main(String[] args) {
         System.out.println("Exemplo de projeto entre Git e NetBeans");
+        System.out.println("Segundo commit via website do github");
     }
     
 }
