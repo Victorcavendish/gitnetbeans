cb57347fe3b2b81efcaae65b40c1748bf2147d9e
Wilson <Wilson@Wilson-PC>
2017-11-26 00:43:41 -0200

    Primeiro commit no netbeans

---
+++ src/gitnetbeans/GitNetBeans.java
@@ -0,0 +1,10 @@
+
+package gitnetbeans;
+
+public class GitNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Exemplo de projeto entre Git e NetBeans");
+    }
+    
+}
